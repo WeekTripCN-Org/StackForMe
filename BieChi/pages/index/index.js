6b6a235c84ca6e1f8036d2ef5819baaa4fef0209
LuckyNine <ricky9102@icloud.com>
2017-03-30 10:43:30 +0800

    UPDATE index

--- BieChi/pages/index/index.js
+++ BieChi/pages/index/index.js
@@ -1,26 +1,123 @@
-//index.js
-//获取应用实例
-var app = getApp()
+const App = getApp()
+
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {}
+    activeIndex: 0,
+    navList: [],
+    indicatorDots: !0,
+    autoplay: !1,
+    current: 0,
+    interval: 3000,
+    duration: 1000,
+    circular: !0,
+    goods: {},
+    prompt: {
+      hidden: !0,
+    },
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+  swiperchange(e) {
+    console.log(e.detail.current)
+  },
+  onLoad() {
+    this.banner = App.HttpResource('/banner/:id', {id:'@id'})
+    this.goods = App.HttpResource('/goods/:id', {id:'@id'})
+    this.classify = App.HttpResource('/classify/:id', {id: '@id'})
+
+    this.getBanners()
+    this.getClassify()
+  },
+  initData() {
+    const type = this.data.goods.params && this.data.goods.params.type || ''
+    const goods= {
+      items: [],
+      params: {
+        page: 1,
+        limit: 10,
+        type: type,
+      },
+      paginate: {}
+    }
+
+    this.setData({
+      goods: goods
     })
   },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
+  navigateTo(e) {
+    console.log(e)
+    App.WxService.navigateTo('/pages/goods/detail/index', {
+      id: e.currentTarget.dataset.id
     })
-  }
+  },
+  search() {
+    App.WxService.navigateTo('/pages/search/index')
+  },
+  getBanners() {
+    this.banner.queryAsync({is_show: !0}).then(data => {
+      console.log(data)
+      if(data.meta.code == 0) {
+        data.data.items.forEach(n => n.path = App.renderImage(n.images[0].path))
+        this.setData({
+          images:data.data.items
+        })
+      }
+    })
+  },
+  getClassify() {
+    const activeIndex = this.data.activeIndex
+    this.classify.queryAsync({
+      page:1,
+      limit:4,
+    }).then(data => {
+      console.log(data)
+      if (data.meta.code == 0) {
+        this.setData({
+          navList: data.data.items,
+          'goods.params.type': data.data.items[activeIndex]._id
+        })
+        this.onPullDownRefresh()
+      }
+    })
+  },
+  getList() {
+    const goods = this.data.goods
+    const params = goods.params
+
+    this.goods.queryAsync(params).then(data => {
+      console.log(data)
+      if(data.meta.code == 0) {
+        data.data.items.forEah(n => n.thumb_url = App.renderImage(n.images[0] && n.images[0].path))
+        goods.items = [...goods.items, ...data.data.items]
+        goods.paginate = data.data.paginate
+        goods.params.page = data.data.paginate.next
+        goods.params.limit = data.data.paginate.perPage
+        this.setData({
+          goods: goods,
+          'prompt.hidden': goods.items.length,
+        })
+      }
+    })
+  },
+  onPullDownRefresh() {
+    console.info('onPullDownRefresh')
+    this.initData()
+    this.getList()
+  },
+  onTapTag(e) {
+    const type = e.currentTarget.dataset.type
+    const index = e.currentTarget.dataset.index
+    const goods = {
+      items: [],
+      params: {
+        page: 1,
+        limit: 10,
+        type: type,
+      },
+      paginate: {}
+    }
+    this.setData({
+      activeIndex:index,
+      goods:goods,
+    })
+    this.getList()
+  },
 })
